6844f7ebea75072c952ea7cec290b25e0bb499d8
IXFI Dev Team <ixfi@blockchainsolutions.network>
2022-05-20 09:18:36 +0530

    Contract Updated

--- Day3/Presale.sol
+++ Day3/Presale.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.6.0;
+import './ERC20.sol';
 
 contract Crowdfunding{
     //Token Information
@@ -8,6 +9,7 @@ contract Crowdfunding{
     uint public totalSupply;
     address public tokenAddress;
     uint public numOfTokenForSale;
+    ERC20 erc20;
 
     //Crowdfunding Information
     uint softCaps;
@@ -20,9 +22,11 @@ contract Crowdfunding{
     uint claimLimit;
     uint perClaimTime;
     string ideaDiscription;
+    uint totalRaised;
 
     mapping(address => uint) public numberOfUsersToken;
     mapping(address => uint) public bonusForUser;
+    mapping(address => uint) public totalClaimedTokensByUser;
 
     constructor(string memory _name,
     string memory _symbol,
@@ -40,7 +44,48 @@ contract Crowdfunding{
     uint _claimLimit,
     uint _perClaimTime,
     string memory _ideaDiscription) public{
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        erc20 = ERC20(_tokenAddress);
+        numOfTokenForSale = _numOfTokenForSale;
+        ideaDiscription = _ideaDiscription;
+        softCaps = _softCaps;
+        hardCaps = _hardCaps;
+        pricePerWei = _pricePerWei;
+        perUserLimit = _perUserLimit;
+        startTime = _startTime;
+        endTime = _endTime;
+        bonus = _bonus;
+        claimLimit = _claimLimit;
+        perClaimTime = _perClaimTime;
+    }
 
+    function buyTokens() payable public {
+        require(erc20.balanceOf(address(this)) > numOfTokenForSale, "Tokens are not supplied to contract!");
+        require(startTime < now, "Presale is not started");
+        require(endTime > now, "Presale is ended");
+        require(numberOfUsersToken[msg.sender] <= perUserLimit, "You have reached your limit");
+        require(msg.value > 0, "Please send some ETH");
+        require(totalRaised < hardCaps, "Presale is completed");
+
+        numberOfUsersToken[msg.sender] = msg.value*pricePerWei;
+        totalRaised = convertWeiToETH(msg.value);
+    }
+
+    function claimToken() public {
+        require(endTime < now, "Presale is not ended");
+        require(numberOfUsersToken[msg.sender] > 0, "You have not tokens to claim");
+        require(totalRaised > softCaps, "Presale is not successful");
+        require(totalClaimedTokensByUser[msg.sender] <= numberOfUsersToken[msg.sender], "Your all the tokens are claimed");
+        uint amountToClaim = numberOfUsersToken[msg.sender] * (claimLimit/100);
+        erc20.transfer(msg.sender,amountToClaim);
+        totalClaimedTokensByUser[msg.sender] = numberOfUsersToken[msg.sender] * (claimLimit/100);
+    }
+
+    function convertWeiToETH(uint number) internal pure returns(uint) {
+        return number / 1 ether;
     }
 
     
